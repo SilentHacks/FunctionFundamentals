b63f17912ddb4cc10baba9b64a6b9d23efe9ce61
SilentHacks <therealsilenthacks@gmail.com>
2022-03-09 14:52:46 +0000

    Added wolframalpha API querying, added user input parsing/validation

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# Hello world
+from wolframalpha import Client
+from dotenv import load_dotenv
+
+
+load_dotenv()
+client = Client.from_env()
+
+
+def main():
+    res = client.query('is f(x)=x^2 surjective')
+    if res['@success']:
+        print(next(res.results).text)
+    else:
+        print(res)
+
+
+if __name__ == '__main__':
+    main()
