ea89d705e057a218c70c7129356f858e7ca7a002
Ajay <ajay.mahendrakumaran@gmail.com>
2022-03-20 17:56:42 +0000

    Now program makes random equations needs to be checked

--- questionGeneration.py
+++ questionGeneration.py
@@ -1,39 +1,51 @@
 import random
 
-
-master = []
 operations = ["*", "/", "+", "-"]
-numbers = ["0.5", "0", "1", "2", "3"]
+numbers = ["0.5","1", "2", "3"]
 
-def tuple():
+def randomElement():
+    # chooses random element from each list
     randomIndexOperation = random.randint(0,len(operations)-1)
     indexOperation = operations[randomIndexOperation]
 
     randomIndexNumber = random.randint(0,len(numbers)-1)
     indexNumber = numbers[randomIndexNumber]
 
+    # pops one out from list
     list = [indexNumber, indexOperation]
-
-    return list
-
-def popper(list):
     randomIndexPopper = random.randint(0,len(list)-1)
     indexElement = list[randomIndexPopper]
 
     return indexElement
 
-def equation(indexElement):
-    ex = "x"
-    power = "^"
+def equation(finalString):
+    indexElement = randomElement()
+    # brackets around equation if necessary
+    if (len(finalString)) > 1:
+        finalString = "(" + finalString + ")"
+
+    # force power to equation
+    if (len(finalString)) > 1:
+        if (finalString[2] or finalString[1]) in operations:
+            randomIndexNumber = random.randint(0,len(numbers)-1)
+            indexNumber = numbers[randomIndexNumber]
+
+            finalString = finalString + "^" + indexNumber
+
+            return finalString   
+        # power
     if indexElement in numbers:
-        finalString = ex + power + indexElement
+        finalString = finalString + "^" + indexElement
         return finalString
+
+        # basic arithmetic
     else:
         randomNumber = str(random.randint(0,100))
-        finalString = ex + indexElement + randomNumber
+        finalString = finalString + indexElement + randomNumber
         return finalString
 
+main = "x"
+for x in range(0,3):
+    main = equation(main)
 
-list = tuple()
-value = popper(list)
-print(equation(value))
+print(main)
